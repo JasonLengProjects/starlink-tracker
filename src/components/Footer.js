aaa98432a97b6021c87d08ddbd1b1f4d2e9a983c
JasonLengProjects <hongze.leng@colorado.edu>
2020-08-18 22:46:23 -0600

    first commit with some input boxes

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                Starlink Tracker. All Rights Reserved. Website Made by Jason Leng
+            </footer>
+        );
+    }
+}
+
+export default Footer;
