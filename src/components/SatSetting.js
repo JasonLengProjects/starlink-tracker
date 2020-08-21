979d51b0759c3049c6efa329dd991e08c28c98bc
JasonLengProjects <hongze.leng@colorado.edu>
2020-08-20 20:35:07 -0600

    fixed altitude & added search radius

--- src/components/SatSetting.js
+++ src/components/SatSetting.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { InputNumber } from 'antd';
+import { InputNumber, Button } from 'antd';
 
 class SatSetting extends Component {
     constructor() {
         super();
         this.state = {
             observerLat: 0,
-            observerLong: 0
+            observerLong: 0,
+            observerAlt: 0,
+            radius: 90
         }
     }
 
@@ -22,6 +24,22 @@ class SatSetting extends Component {
         this.setState({
             observerLat: value
         })
+    }
+
+    onChangeAlt = (value) => {
+        this.setState({
+            observerAlt: value
+        })
+    }
+
+    onChangeRadius = (value) => {
+        this.setState({
+            radius: value
+        })
+    }
+
+    showSatellite = () => {
+        this.props.onShow(this.state);
     }
 
     render() {
@@ -40,7 +58,6 @@ class SatSetting extends Component {
                                 onChange={this.onChangeLong}
                             />
                         </div>
-
                         <div className="list-item right-item">
                             <label>Latitude: </label>
                             <InputNumber
@@ -64,6 +81,30 @@ class SatSetting extends Component {
                                 onChange={this.onChangeEle}
                             />
                         </div>
+                    </div>
+
+                    <p className="setting-label">Restrictions</p>
+                    <div className="setting-list">
+                        <div className="list-item">
+                            <label>Search Radius </label>
+                            <InputNumber
+                                min={0}
+                                max={90}
+                                defaultValue={0}
+                                style={{ margin: "0 2px" }}
+                                onChange={this.onChangeRadius}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="show-nearby">
+                        <Button
+                            className="show-nearby-btn"
+                            size="large"
+                            onClick={this.showSatellite}
+                        >
+                            Find Nearby Satellites
+                            </Button>
                     </div>
                 </div>
             </div>
