e778a12fd0e120a8445257f904cf5728e9a1598c
JasonLengProjects <hongze.leng@colorado.edu>
2020-08-20 22:08:30 -0600

    connected satellite API & styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import SatSetting from "./SatSetting";
+import SatelliteList from "./SatelliteList";
+import { NEARBY_SATELLITE, STARLINK_CATEGORY, API_KEY } from '../constants';
+import Axios from 'axios';
 
 class Main extends Component {
+    constructor() {
+        super();
+        this.state = {
+            setting: null,
+            loadingSatellites: false
+        }
+    }
+
+    // showNearbySatellite = (setting) => {
+    //     this.fetchSatellite(setting);
+    // }
+
+    fetchSatellite = (setting) => {
+        const { observerLat, observerLong, observerAlt, radius } = setting;
+        const url = `${NEARBY_SATELLITE}/${observerLat}/${observerLong}/${observerAlt}/${radius}/${STARLINK_CATEGORY}/&apiKey=${API_KEY}`;
+
+        this.setState({
+            loadingSatellites: true
+        })
+
+        Axios.get(url).then(response => {
+            this.setState({
+                satInfo: response.data,
+                loadingSatellites: false
+            })
+        })
+            .catch(error => {
+                console.log('err in fetch satellite -> ', error);
+                this.setState({
+                    loadingSatellites: false
+                })
+            })
+    }
+
+
+
     render() {
         return (
             <div className='main'>
                 <div className="left-side">
-                    <SatSetting />
+                    <SatSetting onShow={this.fetchSatellite} />
+                    <SatelliteList satInfo={this.state.satInfo} loading={this.state.loadingSatellites} />
                 </div>
                 <div className="right-side">
                     right
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+
+export const API_KEY = "XFMMKX-JL588F-23EHJD-4J0B";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = `${BASE_URL}/above`;
